2625c4e880fc4f310d9d7b6e672b9cf21cc99b1d
loppy33 <nikita.petrenko1406@gmail.com>
2024-07-24 12:29:28 +0200

    123

--- src/pages/home/components/greetings/greetings.jsx
+++ src/pages/home/components/greetings/greetings.jsx
@@ -12,7 +12,7 @@ export default function Greetings() {
         <div className="Greetings">
             <h1 data-aos="fade-left"><span>I'm</span> Nikita</h1>
             <p data-aos="fade-right">A full-stack web developer helping startups turn their visions into digital reality. <br /> I specialize in designing and building modern, responsive websites <br /> without using React, handling both front-end and back-end development <br /> to deliver comprehensive and effective solutions.</p>
-            <a href="/about" data-aos="fade-up">More about me</a>
+            <a href="portfolio/about" data-aos="fade-up">More about me</a>
             <img src="./Avatar.png" alt="" data-aos="fade-down" />
         </div>
     );
