c504706d19f12102506b8a6843599698cf49d6d6
loppy33 <nikita.petrenko1406@gmail.com>
2024-07-24 12:35:31 +0200

    Link

--- src/pages/home/components/greetings/greetings.jsx
+++ src/pages/home/components/greetings/greetings.jsx
@@ -3,6 +3,7 @@ import './greetings.sass';
 
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import { Link } from 'react-router-dom';
 export default function Greetings() {
     useEffect(() => {
         AOS.init({ duration: 1000 });
@@ -12,7 +13,7 @@ export default function Greetings() {
         <div className="Greetings">
             <h1 data-aos="fade-left"><span>I'm</span> Nikita</h1>
             <p data-aos="fade-right">A full-stack web developer helping startups turn their visions into digital reality. <br /> I specialize in designing and building modern, responsive websites <br /> without using React, handling both front-end and back-end development <br /> to deliver comprehensive and effective solutions.</p>
-            <a href="/portfolio/about" data-aos="fade-up">More about me</a>
+            <Link to="/portfolio/about" data-aos="fade-up">More about me</Link>
             <img src="./Avatar.png" alt="" data-aos="fade-down" />
         </div>
     );
